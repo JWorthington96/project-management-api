812f2e9bff509b97d77707268d32be48ecb88c55
Joshua Worthington <codingjosh@gmail.com>
2018-11-14 14:28:44 +0000

    removed future feature reference that broke the app

--- project-management-client/src/Routes.js
+++ project-management-client/src/Routes.js
@@ -5,7 +5,6 @@ import Login from "./containers/Login";
 import Register from "./containers/Register";
 import NewProject from "./containers/NewProject";
 import Project from "./containers/Project";
-import Account from "./containers/Account";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 
@@ -16,6 +15,5 @@ export default ({childProps}) =>
         <AppliedRoute path="/register" exact component={Register} props={childProps} />
         <AppliedRoute path="/projects/new" exact component={NewProject} props={childProps} />
         <AppliedRoute path="/projects/:id" exact component={Project} props={childProps} />
-        <AppliedRoute path="/account" exact component={Account} props={childProps} />
         <Route component={NotFound} />
     </Switch>
