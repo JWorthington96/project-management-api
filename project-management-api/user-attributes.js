0169742087cf5acf5adf7de42e4dc8937340b8a0
Josh <codingjosh@gmail.com>
2018-11-24 20:40:08 +0000

    added Lambda functions to get and modify user attributes

---
+++ project-management-api/user-attributes.js
@@ -0,0 +1,26 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { call } from "./lib/cognito";
+import { success, failure } from "./lib/response";
+export function main(event, context, callback) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const params = {
+            AccessToken: event.queryStringParameters.AccessToken
+        };
+        try {
+            const user = yield call('getUser', params);
+            callback(null, success({ status: true, body: user }));
+        }
+        catch (error) {
+            console.log(error);
+            callback(null, failure({ status: false, body: error.message }));
+        }
+    });
+}
+//# sourceMappingURL=user-attributes.js.map
