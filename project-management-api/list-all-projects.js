0b4b38ec805a5fdebfd101106bbb8f570280238a
Josh <codingjosh@gmail.com>
2018-11-29 22:36:26 +0000

    added new functions to serverless.xml and one to list all projects

---
+++ project-management-api/list-all-projects.js
@@ -0,0 +1,26 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { call } from "./lib/dynamodb";
+import { success, failure } from "./lib/response";
+export function main(event, context, callback) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const params = {
+            TableName: "projects"
+        };
+        try {
+            const response = yield call('scan', params);
+            callback(null, success({ status: true, body: response.Items }));
+        }
+        catch (error) {
+            console.log(error);
+            callback(null, failure({ status: false, body: error }));
+        }
+    });
+}
+//# sourceMappingURL=list-all-projects.js.map
